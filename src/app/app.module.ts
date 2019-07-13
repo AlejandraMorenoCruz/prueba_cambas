e9dfd40c0a4e9ed96fe4f087bd300ef0f79c4ee7
Alejandra.Moreno <ale.mocr@gmail.com>
2019-07-12 21:45:33 -0500

    esqueleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PostComponent } from './components/post/post.component';
+import { PostDetalleComponent } from './components/post-detalle/post-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    PostComponent,
+    PostDetalleComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/post-detalle/post-detalle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-detalle',
+  templateUrl: './post-detalle.component.html',
+  styleUrls: ['./post-detalle.component.css']
+})
+export class PostDetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
